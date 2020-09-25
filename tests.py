9acb8c1ec12ea5001eea1e005efa95fb618b2f35
Nabil Bennani <bennani.kerrout.nabil@gmail.com>
2020-09-25 18:20:16 +0100

    UI Widgets added from QtDesigner Builds

--- tests.py
+++ tests.py
@@ -2,7 +2,6 @@ import sys
 
 from PyQt5.QtWidgets import QDialog, QApplication
 from templates.ui.CustomerSearch import Ui_CustomerSearchWidget
-from.models import Customer
 
 
 class CustomerSearchDialog(QDialog):
@@ -10,10 +9,18 @@ class CustomerSearchDialog(QDialog):
         super(CustomerSearchDialog, self).__init__()
 
         # Set up the user interface from Designer.
-        self.initUI()
-
-        # Here connect functionalities
-
-    def initUI(self):
         self.ui = Ui_CustomerSearchWidget()
         self.ui.setupUi(self)
+
+        # Connect up the buttons.
+        # self.ui.okButton.clicked.connect(self.accept)
+        # self.ui.cancelButton.clicked.connect(self.reject)
+
+
+app = QApplication(sys.argv)
+window = CustomerSearchDialog()
+# ui = CustomerSearchDialog()
+# ui.setupUi(window)
+
+window.show()
+sys.exit(app.exec_())
