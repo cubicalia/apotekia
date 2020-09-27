9f750dc8c32fed9af559e7f6eaba9c26dcf301d1
Nabil Bennani <bennani.kerrout.nabil@gmail.com>
2020-09-27 13:02:49 +0100

    refactoring the POS widgets

--- pos/views.py
+++ pos/views.py
@@ -4,17 +4,25 @@ from PyQt5.QtWidgets import QGridLayout, QHBoxLayout, QWidget
 
 from customers.views import CustomerSearchDialog
 from catalog.views import ProductSearchDialog
+from sales.views import BasketPOSDialog, DisplayPOSTotalsDialog
 
 
 class POS(QWidget):
     def __init__(self):
         super(POS, self).__init__()
         self.layout = QGridLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
+        # Initiaite Widgets
         self.product_search = ProductSearchDialog()
         self.customer_search = CustomerSearchDialog()
+        self.basket_widget = BasketPOSDialog()
+        self.totals_widget = DisplayPOSTotalsDialog()
 
-        self.layout.addWidget(self.product_search, 1, 1, 1, 1)
+        # Layout Widgets
+        self.layout.addWidget(self.product_search, 1, 1, 1, 2)
         self.layout.addWidget(self.customer_search, 2, 1, 1, 1)
+        self.layout.addWidget(self.basket_widget, 1, 3, 2, 1)
+        self.layout.addWidget(self.totals_widget, 2, 2, 1, 1)
 
         self.setLayout(self.layout)
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from apotekia import db_setup
+import sys
 
-# Create your views here.
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.QtCore import Qt, QSortFilterProxyModel, pyqtSlot
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from templates.ui.BasketWidget import Ui_Form
+from templates.ui.TotalsFormWidget import Ui_TotalsWidget
+
+from sales.models import Basket, BasketLine
+
+
+class BasketPOSDialog(QWidget):
+    def __init__(self):
+        super(BasketPOSDialog, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+
+class DisplayPOSTotalsDialog(QWidget):
+    def __init__(self):
+        super(DisplayPOSTotalsDialog, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.ui = Ui_TotalsWidget()
+        self.ui.setupUi(self)
