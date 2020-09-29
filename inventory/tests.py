2456a8dad579f561a5f0e85ce75cfadf95931509
Nabil Bennani <bennani.kerrout.nabil@gmail.com>
2020-09-29 13:37:59 +0100

    refactoring the POS widgets

--- inventory/tests.py
+++ inventory/tests.py
@@ -12,19 +12,16 @@ from templates.ui.InventoryWidget import Ui_StockWidget
 class InventoryDialog(QDialog):
     def __init__(self):
         super(InventoryDialog, self).__init__()
-
         self.locations = InventoryLocation.objects.all()
         self.model = QStandardItemModel(len(self.locations), 1)
         self.model.setHorizontalHeaderLabels(['Location'])
         self.show_locations()
-
         self.initUI()
 
     def initUI(self):
         self.ui = Ui_StockWidget()
         self.ui.setupUi(self)
         self.ui.StockLocationList.setModel(self.model)
-
         self.selection_model = self.ui.StockLocationList.selectionModel()
         self.selection_model.selectionChanged.connect(self.on_selectionChanged)
 
@@ -39,7 +36,6 @@ class InventoryDialog(QDialog):
             self.ui.StockMovementsTable.setItem(row, 7, QTableWidgetItem(str(entry.qty)))
 
     def show_locations(self):
-
         for row, location in enumerate(self.locations):
             # print(str(location))
             item = QStandardItem(str(location))
@@ -54,8 +50,6 @@ class InventoryDialog(QDialog):
                 self.ui.StockMovementsTable.update()
                 self.show_movements(item.data())
                 print(item.data())
-
-
 
 
 if __name__ == "__main__":
