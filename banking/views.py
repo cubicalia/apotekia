50e7bc109138ce4e765916e41de1414fde34a2ec
Nabil Bennani <bennani.kerrout.nabil@gmail.com>
2020-10-04 21:06:21 +0100

    Banking APP Added

--- banking/views.py
+++ banking/views.py
@@ -9,10 +9,23 @@ from banking.models import BankAccount
 class BankingDialog(QDialog):
     def __init__(self):
         super(BankingDialog, self).__init__()
-        self.banks = BankAccount.objects.all()
+
+        self.accounts = BankAccount.objects.all()
+        self.model = QStandardItemModel(len(self.accounts), 1)
+        self.model.setHorizontalHeaderLabels(['Accounts', 'Balance'])
+        self.populate_model()
 
         self.initUI()
 
     def initUI(self):
         self.ui = Ui_Banking()
         self.ui.setupUi(self)
+        self.ui.AccountsTableView.setModel(self.model)
+
+    def populate_model(self):
+        for row, account in enumerate(self.accounts):
+            print(str(account))
+            item = QStandardItem(str(account.account_name))
+            balance = QStandardItem(str(account.current_balance))
+            self.model.setItem(row, 0, item)
+            self.model.setItem(row, 1, balance)
